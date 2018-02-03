a4c5f4a43ab9ace1a811d826c3e89926bc415d06
jeffr <jeffr@DESKTOP-NP8BT8O>
2018-02-03 19:42:11 +0100

    added a try/catch exception to the driver Program class

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in); // create input scanner
+		int myAge;
+		
+		try {
+			
+			System.out.println("Please enter your age: ");
+			myAge = input.nextInt();
+			System.out.println("\nYou entered: " + myAge);
+			
+		} catch (Exception e) {
+			System.out.println("Please enter a valid number.");
+		}
 
 	}
 
