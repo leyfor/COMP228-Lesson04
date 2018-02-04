c21999d7e0bef2e462e79ff40c51b0ec5bb0cc83
leyfor <justman51@yahoo.com>
2018-02-04 13:09:49 +0100

    added person class

--- src/Program.java
+++ src/Program.java
@@ -5,13 +5,33 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Person person = new Person();
-		
-		//int myAge;
-		//boolean isValid = false;
-		// Nothing...
-		
-		while (!person.getIsValid()) {
+		String  prompt = "";
+		do {
+			System.out.println("Please Make a Selection");
+			System.out.println("-------------------");
+			System.out.println("1. Enter Your Age");
+			System.out.println("2. Exit");
+			System.out.println("-------------------");
+			Scanner input = new Scanner(System.in);
+			prompt  = input.nextLine();
+			switch (prompt) {
+			case "1":
+				setAge(person);
+				break;
+			case "2":
+				break;
+
+			}
 			
+			clearConsole();
+		}while(!prompt.equals("2"));
+		
+
+	}
+  
+	// Set person's age
+	private static void setAge(Person person) {
+		while (!person.getIsValid()) {
 			
 				Scanner input = new Scanner(System.in); // create input scanner
 				
@@ -23,18 +43,25 @@ public class Program {
 				} else {
 					System.out.println();
 				}
-			
-			
-				
-				
-				
-				
-			
-			
+	
 		}
-		
-		
-
+	}
+	
+	
+	// Clear console
+	public final static void clearConsole() {
+		try {
+			final String os = System.getProperty("os.name");
+			
+			if(os.contains("Windows")) {
+				Runtime.getRuntime().exec("cls");
+			} else {
+				Runtime.getRuntime().exec("clear");
+			}
+			
+		} catch (final Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 }
