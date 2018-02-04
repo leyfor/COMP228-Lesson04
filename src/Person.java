50cfa1c497a3fda245a7a189e5f8da940b9674d7
leyfor <justman51@yahoo.com>
2018-02-04 19:46:17 +0100

    updated the Person class with constructor overloading

--- src/Person.java
+++ src/Person.java
@@ -1,56 +1,70 @@
 
 public class Person {
-	private int _age;
-	private boolean _isValid = false;
+    private int _age;
+    private float _weight;
+    private double _IQ;
+    private boolean _isValid = false;
 
-	// GET ACCESSOR METHOD for age
-	public int getAge() {
-		return _age;
-	}
-
-	
-	// SET MUTATOR METHOD for age
-	public void setAge(String ageString) {
-		
-		try {
-			int age = Integer.parseInt(ageString);
-			
-			if(age < 0) {
-	        	System.out.println("Please enter a positive number");
-	        } else {
-	        	this._age = age;
-	        	this._isValid = true;
-	        }
-			
-		} catch (Exception exception) {
-			System.out.println("Please enter an integer value");
-		}
-		
-        
-		
-	}
-	
-	// Empty CONSTRUCTOR
-	Person() {
-	
-		
-	}
-	
-	// OVERIDDING TOSTRING METHOD
-	public String toString() {
-		return "\nYou entered: " + this._age;
-		
-	}
+    // GET ACCESSOR METHOD for age
+    public int getAge() {
+        return _age;
+    }
 
 
-	// GET ACCESSOR METHOD for isValid
-	public boolean getIsValid() {
-		return this._isValid;
-	}
+    // SET MUTATOR METHOD for age
+    public void setAge(String ageString) {
+
+        try {
+            int age = Integer.parseInt(ageString);
+
+            if (age < 0) {
+                System.out.println("Please enter a positive number");
+            } else {
+                this._age = age;
+                this._isValid = true;
+            }
+
+        } catch (Exception exception) {
+            System.out.println("Please enter an integer value");
+        }
 
 
-	
-	
-	
+    }
+
+    // Empty CONSTRUCTOR
+    Person() {
+
+    }
+
+    // CONSTRUCTOR that sets age
+    Person(int age) {
+        this._age = age;
+
+    }
+
+    // CONSTRUCTOR that sets weight
+    Person(float weight) {
+        this._weight = weight;
+        System.out.println("Weight: " + this._weight);
+    }
+
+    // CONSTRUCTOR that sets IQ
+    Person(double IQ) {
+        this._IQ = IQ;
+        System.out.println("Weight: " + this._IQ);
+    }
+
+    // OVERIDDING TOSTRING METHOD
+    public String toString() {
+        return "\nYou entered: " + this._age;
+
+    }
+
+
+    // GET ACCESSOR METHOD for isValid
+    public boolean getIsValid() {
+        return this._isValid;
+    }
+
 
 }
--- src/Program.java
+++ src/Program.java
@@ -2,66 +2,66 @@ import java.util.Scanner;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
-		Person person = new Person();
-		String  prompt = "";
-		do {
-			System.out.println("Please Make a Selection");
-			System.out.println("-------------------");
-			System.out.println("1. Enter Your Age");
-			System.out.println("2. Exit");
-			System.out.println("-------------------");
-			Scanner input = new Scanner(System.in);
-			prompt  = input.nextLine();
-			switch (prompt) {
-			case "1":
-				setAge(person);
-				break;
-			case "2":
-				break;
+    public static void main(String[] args) {
 
-			}
-			
-			clearConsole();
-		}while(!prompt.equals("2"));
-		
+        Person person = new Person(1.0);
+        String prompt;
+        do {
+            System.out.println("Please Make a Selection");
+            System.out.println("-------------------");
+            System.out.println("1. Enter Your Age");
+            System.out.println("2. Exit");
+            System.out.println("-------------------");
+            Scanner input = new Scanner(System.in);
+            prompt = input.nextLine();
+            switch (prompt) {
+                case "1":
+                    setAge(person);
+                    break;
+                case "2":
+                    break;
 
-	}
-  
-	// Set person's age
-	private static void setAge(Person person) {
-		while (!person.getIsValid()) {
-			
-				Scanner input = new Scanner(System.in); // create input scanner
-				
-				System.out.println("Please enter your age: ");
-				person.setAge(input.nextLine());
-				
-				if(person.getIsValid()) {
-					System.out.println(person.toString());
-				} else {
-					System.out.println();
-				}
-	
-		}
-	}
-	
-	
-	// Clear console
-	public final static void clearConsole() {
-		try {
-			final String os = System.getProperty("os.name");
-			
-			if(os.contains("Windows")) {
-				Runtime.getRuntime().exec("cls");
-			} else {
-				Runtime.getRuntime().exec("clear");
-			}
-			
-		} catch (final Exception e) {
-			// TODO: handle exception
-		}
-	}
+            }
+
+            clearConsole();
+        } while (!prompt.equals("2"));
+
+
+    }
+
+    // Set person's age
+    private static void setAge(Person person) {
+        while (!person.getIsValid()) {
+
+            Scanner input = new Scanner(System.in); // create input scanner
+
+            System.out.println("Please enter your age: ");
+            person.setAge(input.nextLine());
+
+            if (person.getIsValid()) {
+                System.out.println(person.toString());
+            } else {
+                System.out.println();
+            }
+
+        }
+    }
+
+
+    // Clear console
+      private static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+
+        } catch (final Exception e) {
+            // TODO: handle exception
+        }
+    }
 
 }
